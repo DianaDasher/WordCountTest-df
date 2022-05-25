1e6364503b5000275fdb375f90101d993c36c02c
duanfei <1521368254@qq.com>
2022-05-25 22:10:09 +0800

    firstone

--- 实践一.cpp
+++ 实践一.cpp
@@ -3,38 +3,41 @@
 int main (int argc, char *argv[])
 {
     FILE *fp;
-    int tool = 0;
+    int sum= 0;
     int i; 
-    if((fp=fopen(argv[2], "r")) == NULL)
+    if((fp=fopen(argv[2], "r"))== NULL)
     {
-        puts("error!\n");
+        puts("文件打开失败!\n");
         return 0;
-    }
-    if(argv[1][1] =='w') // 单词个数 
+    }  //判断文件是否成功打开 
+   
+    if(argv[1][1] =='w') // 若在命令行输入w则统计单词个数 
     {
-        char s[1024];
+        char word[10000];
         int x;
-        while (fscanf(fp,"%s",s) != EOF)
+        
+        while ((fscanf(fp,"%s",word))!= EOF)
         {
-            tool ++;
-            for (i=1;i<strlen(s)-1; i++)
-                if (s[i]==','&&s[i-1]!=','&&s[i+1]!=',') 
+            sum++;
+            int l=strlen(word)-1;
+            for (i=1;i<l; i++)
+                if (word[i-1]!=','&&word[i]==','&&word[i+1]!=',') 
                 {
-                    tool ++;
-                }
+                    sum++;
+                }//若字符i为逗号，他的前后字符都不是逗号，则是一个单词 
         }
-        printf("单词数=%d\n",tool);
+        printf("单词数=%d\n",sum);
     }
-    else if(argv[1][1] == 'c') // 字符个数 
+    else if(argv[1][1] == 'c') // 若在命令行输入c则字符个数 
     {
         char c;
-        while ((c = fgetc(fp)) != EOF) 
+        while ((c=fgetc(fp))!= EOF) 
         {
-		 tool ++;
+		 sum++;
 	    }
-        printf("字符数=%d", tool);
+        printf("字符数=%d",sum);
     }
-    fclose(fp);
+    fclose(fp);//关闭文件 
     return 0;
 }
 
