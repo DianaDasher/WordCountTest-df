ceb0dfc277f3c80734318bceebdbd2774b66c03c
duanfei <1521368254@qq.com>
2022-05-25 22:37:30 +0800

    firstone

--- 实践一.cpp
+++ 实践一.cpp
@@ -3,19 +3,19 @@
 int main (int argc, char *argv[])
 {
     FILE *fp;
-    int sum= 0;
+    int sum= 0; 
     int i; 
+    char choose; 
     if((fp=fopen(argv[2], "r"))== NULL)
     {
         puts("文件打开失败!\n");
         return 0;
     }  //判断文件是否成功打开 
-   
-    if(argv[1][1] =='w') // 若在命令行输入w则统计单词个数 
+    choose=argv[1][1];
+    
+    if(choose=='w') // 若在命令行输入w则统计单词个数 
     {
-        char word[10000];
-        int x;
-        
+        char word[10000];    
         while ((fscanf(fp,"%s",word))!= EOF)
         {
             sum++;
@@ -28,7 +28,7 @@ int main (int argc, char *argv[])
         }
         printf("单词数=%d\n",sum);
     }
-    else if(argv[1][1] == 'c') // 若在命令行输入c则字符个数 
+    else if(choose== 'c') // 若在命令行输入c则字符个数 
     {
         char c;
         while ((c=fgetc(fp))!= EOF) 
